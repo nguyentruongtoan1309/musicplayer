b50901394a3b7c99afc291144a5a56abef7b24be
Toan Nguyen Truong <toannguyentruong@itrvn.com>
2022-10-01 21:35:11 +0700

    update access api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -12,6 +12,7 @@ export type AmplifyDependentResourcesAttributes = {
     },
     "api": {
         "musicplayer": {
+            "GraphQLAPIKeyOutput": "string",
             "GraphQLAPIIdOutput": "string",
             "GraphQLAPIEndpointOutput": "string"
         }
